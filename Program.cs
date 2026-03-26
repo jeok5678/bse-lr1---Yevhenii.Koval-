0bdf4f4783770ff3335229351a9a1cc1e5a131a5
evg <yevhenii.koval1@nure.ua>
2026-03-26 23:18:10 +0200

    Initial commit: додано README.md та .gitignore

---
+++ Program.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+//
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public LinkedList()
+    {
+        head = null;
+    }
+
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void Remove(T data)
+    {
+        if (head == null) return;
+
+        if (head.Data.Equals(data))
+        {
+            head = head.Next;
+            return;
+        }
+
+        Node<T> current = head;
+        while (current.Next != null)
+        {
+            if (current.Next.Data.Equals(data))
+            {
+                current.Next = current.Next.Next;
+                return;
+            }
+            current = current.Next;
+        }
+    }
+
+    public void Display()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+
+    public bool Contains(T data)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            if (current.Data.Equals(data))
+                return true;
+            current = current.Next;
+        }
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Display();
+    }
+}
